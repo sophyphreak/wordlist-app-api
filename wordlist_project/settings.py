f2e6c00c94c5e8eeed6ad459759227663a194c9b
Andrew Horn <arhorn@smcm.edu>
2019-06-16 00:22:00 -0500

    added user authorization functionality

--- wordlist_project/settings.py
+++ wordlist_project/settings.py
@@ -25,9 +25,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd-party apps
     'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_auth',
+    'rest_auth.registration',
 
     # Local
     'words.apps.WordsConfig',
@@ -36,7 +43,11 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 MIDDLEWARE = [
@@ -125,3 +136,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # Activate Django-Heroku.
 import django_heroku
 django_heroku.settings(locals())
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
--- wordlist_project/urls.py
+++ wordlist_project/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('words.urls')),
     path('api-auth', include('rest_framework.urls')),
+    path('api/v1/rest-auth/', include('rest_auth.urls')),
+    path('api/v1/rest-auth/registration/',
+        include('rest_auth.registration.urls')),
 ]
